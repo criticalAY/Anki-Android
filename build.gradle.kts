51bb66f338d2a10042f5ca2470e2f33e7d4fce93
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2026-04-09 16:03:33 +0530

    feat: add baseline profile plugin and macrobenchmark library

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,8 @@ import kotlin.system.exitProcess
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.androidx.baselineprofile) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.kotlin.jvm) apply false
