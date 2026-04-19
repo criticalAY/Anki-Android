criticalAY/Anki-Android

commit cd49b402e0cdc1b52164dc0b65be200eaa8dd740

feat(dialog): add input validation
Add a 'validator' parameter which can display an error

Co-authored-by: vinay-singh-dev <vv4011273@gmail.com>

AnkiDroid/src/main/java/com/ichi2/utils/AlertDialogFacade.kt
@@ -52,6 +52,30 @@ import timber.log.Timber
 /** Wraps [DialogInterface.OnClickListener] as we don't need the `which` parameter */
 typealias DialogInterfaceListener = (DialogInterface) -> Unit
 
+/**
+ * - [ValidationResult.VALID] - user may proceed
+ * - [ValidationResult.REJECTED] - user may not proceed (no error)
+ * - [ValidationResult.error] - `error` is displayed to the user
+ */
+@JvmInline
+value class ValidationResult private constructor(
+    val error: String?,
+) {
+    companion object {
+        /** The user may proceed */
+        val VALID = ValidationResult(null)
+
+        /**
+         * The user may not proceed; no error displayed
+         *
+         * Typically for 'obvious' issues, such as not changing a name
+         */
+        val REJECTED = ValidationResult("")
+
+        fun error(message: String) = ValidationResult(message)
+    }
+}
+
 fun DialogInterfaceListener.toClickListener(): OnClickListener = OnClickListener { dialog: DialogInterface, _ -> this(dialog) }
 
 /*
@@ -301,6 +325,7 @@ fun AlertDialog.Builder.customListAdapterWithDecoration(
  * @param maxLength if set, the user may not enter more than the supplied number of digits
  * @param inputType see [EditText.setInputType]
  * @param waitForPositiveButton MaterialDialog compat: if `false` [callback] is called on input
+ * @param validator see [ValidationResult]. Valid if `null`, an error is shown if non-null.
  * if `true` [callback] is called when [positiveButton] is pressed
  */
 fun AlertDialog.input(
@@ -311,6 +336,7 @@ fun AlertDialog.input(
     maxLength: Int? = null,
     displayKeyboard: Boolean = false,
     waitForPositiveButton: Boolean = true,
+    validator: ((String) -> ValidationResult)? = null,
     callback: (AlertDialog, CharSequence) -> Unit,
 ): AlertDialog {
     // Builder.setView() may not be called before show()
@@ -325,25 +351,33 @@ fun AlertDialog.input(
 
         inputType?.let { this.inputType = it }
 
-        if (!waitForPositiveButton) {
-            doOnTextChanged { text, _, _, _ ->
-                callback(this@input, text ?: "")
+        doOnTextChanged { text, _, _, _ ->
+            val input = text?.toString() ?: ""
+
+            // handle allowEmpty
+            if (!allowEmpty && input.isEmpty()) {
+                this@input.getInputTextLayout().error = null
+                this@input.positiveButton.isEnabled = false
+                return@doOnTextChanged
             }
-        } else {
-            positiveButton.setOnClickListener { callback(this@input, this.text.toString()) }
+
+            // handle validation errors
+            val validationError = validator?.invoke(input)
+            this@input.getInputTextLayout().error = validationError?.error
+            this@input.positiveButton.isEnabled = validationError == null
+            if (validationError != null) return@doOnTextChanged
+
+            // no errors, see if we should fire the callback on every keypress
+            // TODO: this was used to perform additional validation, which should be moved to the
+            //  'validator' parameter,
+            if (!waitForPositiveButton) {
+                callback(this@input, input)
+            }
         }
 
-        if (!allowEmpty) {
-            // this is called after callback() so allowEmpty takes priority
-            doOnTextChanged { text, _, _, _ ->
-                if (waitForPositiveButton) {
-                    // this is the only validation filter we apply - toggle on or off
-                    this@input.positiveButton.isEnabled = !text.isNullOrEmpty()
-                } else if (text.isNullOrEmpty()) {
-                    // potentially other filters in `waitForPositiveButton`.
-                    // WARN: this could be buggy as it does not toggle the button back on
-                    this@input.positiveButton.isEnabled = false
-                }
+        if (waitForPositiveButton) {
+            positiveButton.setOnClickListener {
+                callback(this@input, this.text.toString())
             }
         }
 

commit c5d9bbdfd7e7248bf34d4e6589cc2ea53792dbc7

fix(dialog): enable 'positive' button
Broken in cd49b402e0cdc1b52164dc0b65be200eaa8dd740

AnkiDroid/src/main/java/com/ichi2/utils/AlertDialogFacade.kt
@@ -364,7 +364,7 @@ fun AlertDialog.input(
             // handle validation errors
             val validationError = validator?.invoke(input)
             this@input.getInputTextLayout().error = validationError?.error
-            this@input.positiveButton.isEnabled = validationError == null
+            this@input.positiveButton.isEnabled = validationError?.error == null
             if (validationError != null) return@doOnTextChanged
 
             // no errors, see if we should fire the callback on every keypress